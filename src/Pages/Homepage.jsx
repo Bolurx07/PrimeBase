ac4c4788f6c4fdae7846aea67b7ea09b28e31777
Israel <iaboluwatife@gmail.com>
2023-08-03 11:53:39 +0100

    fetched raw data from the api

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const Homepage = () => {
+
+    const [user, setUser] = useState();
+    
+    useEffect(() => {
+        const getUserData = () => {
+            try {
+                axios
+                .get('https://api.postman.com/collections/17034879-eedd174f-d366-4d21-a137-38b5bce9d1e0?access_key=PMAT-01H6XA4YB57HM3C1F2A9Y0HTSG')
+                .then((result) => {
+                        const singleUser = result;
+                        console.log(singleUser);
+                        // setUser(singleUser);
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        
+        getUserData()
+    }, []);
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Homepage
